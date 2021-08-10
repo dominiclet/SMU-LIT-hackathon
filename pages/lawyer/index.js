17a9de65b8aac25eb6a149bac33271079a00bcb9
dominiclet <dominic.let@hotmail.com>
2021-08-10 16:47:31 +0800

    color and average cost

--- pages/lawyer/index.js
+++ pages/lawyer/index.js
@@ -35,10 +35,23 @@ const lawyerDashboard = () => {
 			<div className={styles.rowContainer}>
 				<Card className={styles.smallCard}>
 					<Card.Body>
-						<Card.Title>Average cost</Card.Title>
-						<Card.Text>
-							blablabla
-						</Card.Text>
+						<Card.Title>Average cost...</Card.Title>
+						<div className={styles.averageCostInnerInfo}>
+							<div className={styles.averageCostInnerTitle}>
+								of this lawyer:
+							</div>
+							<div className={styles.averageCostInnerPrice}>
+								$80
+							</div>
+						</div>
+						<div className={styles.averageCostInnerInfo}>
+							<div className={styles.averageCostInnerTitle}>
+								of this type of service:
+							</div>
+							<div className={styles.averageCostInnerPrice}>
+								$100
+							</div>
+						</div>
 					</Card.Body>
 				</Card>
 			</div>
