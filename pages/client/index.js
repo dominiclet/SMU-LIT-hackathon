33ca30fb63257dbafde6372a2df98b6a30a715f3
dominiclet <dominic.let@hotmail.com>
2021-08-13 15:17:54 +0800

    added logout

--- pages/client/index.js
+++ pages/client/index.js
@@ -7,26 +7,38 @@ import { CaseBriefView } from "../../components/clientDashboard/CaseBriefView";
 import CaseBriefEdit from "../../components/CaseBriefEdit";
 import AverageCost from "../../components/AverageCost";
 import withAuthClient from "../../components/authenticationHelpers/withAuthClient";
+import { BoxArrowLeft } from "react-bootstrap-icons";
+import { useRouter } from "next/dist/client/router";
 
 const clientDashboard = () => {
+	const router = useRouter();
+	// Logout function
+	const handleLogout = () => {
+		localStorage.removeItem("jwt-token");
+		router.push("/login");
+	}
+
 	return (
-		<div className={styles.outerContainer}>
-			<StageStepper/>
-			<div className={styles.rowContainer}>
-				<LawyerProfile/>
-				<MeetUpInfo/>
-			</div>
-			<div className={styles.rowContainer}>
-				<CaseBriefView/>
-			</div>
-			<div className={styles.rowContainer}>
-				<CaseBriefEdit/>
-			</div>
-			<div className={styles.rowContainer}>
-				<AverageCost/>
-			</div>
-			<SelectionButtons/>
-		</div>	
+		<>
+			<BoxArrowLeft onClick={handleLogout} className={styles.logoutIcon} size="35" />
+			<div className={styles.outerContainer}>
+				<StageStepper/>
+				<div className={styles.rowContainer}>
+					<LawyerProfile/>
+					<MeetUpInfo/>
+				</div>
+				<div className={styles.rowContainer}>
+					<CaseBriefView/>
+				</div>
+				<div className={styles.rowContainer}>
+					<CaseBriefEdit/>
+				</div>
+				<div className={styles.rowContainer}>
+					<AverageCost/>
+				</div>
+				<SelectionButtons/>
+			</div>	
+		</>
 	)
 }
 
--- pages/lawyer/index.js
+++ pages/lawyer/index.js
@@ -6,8 +6,11 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { apiRoot } from "../../config";
 import withAuthLawyer from "../../components/authenticationHelpers/withAuthLawyer";
+import { BoxArrowLeft } from "react-bootstrap-icons";
+import { useRouter } from "next/dist/client/router";
 
 const lawyerDashboardPage = () => {
+	const router = useRouter();
 	const [clientList, setClientList] = useState();
 
 	useEffect(() => {
@@ -19,29 +22,38 @@ const lawyerDashboardPage = () => {
 	}, []);
 
 	if (clientList) {
+		// Logout button
+		const handleLogout = () => {
+			localStorage.removeItem("jwt-token");
+			router.push("/login");
+		}
+
 		return (
-			<Tab.Container defaultActiveKey={0}>
-				<div className={styles.mainContainer}>
-					<Nav variant="pills" className={styles.navbarContainer}>
-						{clientList.map((client, index) => {
-							return (
-								<Nav.Item className={styles.navItem}>
-									<Nav.Link eventKey={index}>{client.name}</Nav.Link>
-								</Nav.Item>
-							);
-						})}
-					</Nav>
-					<Tab.Content>
-						{clientList.map((client, index) => {
-							return (
-								<Tab.Pane eventKey={index}>
-									<LawyerDashboard clientId={client.id} stage={client.stage} />
-								</Tab.Pane>
-							)
-						})}
-					</Tab.Content>
-				</div>
-			</Tab.Container>
+			<div>
+				<BoxArrowLeft size="35" className={styles.logoutIcon} onClick={handleLogout} />
+				<Tab.Container defaultActiveKey={0}>
+					<div className={styles.mainContainer}>
+						<Nav variant="pills" className={styles.navbarContainer}>
+							{clientList.map((client, index) => {
+								return (
+									<Nav.Item className={styles.navItem}>
+										<Nav.Link eventKey={index}>{client.name}</Nav.Link>
+									</Nav.Item>
+								);
+							})}
+						</Nav>
+						<Tab.Content>
+							{clientList.map((client, index) => {
+								return (
+									<Tab.Pane eventKey={index}>
+										<LawyerDashboard clientId={client.id} stage={client.stage} />
+									</Tab.Pane>
+								)
+							})}
+						</Tab.Content>
+					</div>
+				</Tab.Container>
+			</div>
 		);
 	} else {
 		return (
